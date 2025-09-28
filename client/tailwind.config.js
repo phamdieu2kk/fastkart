4b36c0e0db5280d7b0254cda94e07e88180782c4
Pham Dieu <phamdieu1503@gmail.com>
2025-09-28 12:18:37 +0700

    fix all

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -7,15 +7,24 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Roboto', 'sans-serif'], // Dùng Roboto cho tất cả
+        sans: ['Roboto', 'sans-serif'], // Dùng Roboto cho toàn bộ app
       },
-        screens: {
-    'xs': '480px',
-    'sm': '640px',
-    'md': '768px',
-    'lg': '1024px',
-    'xl': '1280px',
-  },
+      keyframes: {
+        "slide-down": {
+          "0%": { transform: "translateY(-100%)", opacity: 0 },
+          "100%": { transform: "translateY(0)", opacity: 1 },
+        },
+      },
+      animation: {
+        "slide-down": "slide-down 0.5s ease-out forwards",
+      },
+      screens: {
+        'xs': '480px',
+        'sm': '640px',
+        'md': '768px',
+        'lg': '1024px',
+        'xl': '1280px',
+      },
     },
   },
   plugins: [],
